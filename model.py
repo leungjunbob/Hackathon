182bf47fb063704ae079df4b1da02254ebc03327
leungjunbob <141125388+leungjunbob@users.noreply.github.com>
2024-09-24 18:31:57 +1000

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,21 @@
+class Model():
+    def __init__(self) -> None:
+        pass
+    
+    def roomTemperatureSetting(self, roomTemperature=None, targetTemperature=None, mode="Cooling"):
+        if mode == "Cooling":
+            startTime, acTemperature, energyConsumption = self.function1(roomTemperature, targetTemperature)
+        if mode == "heating":
+            pass
+        return startTime, acTemperature, energyConsumption
+    
+    def function1(self, roomTemperature, targetTemperature):
+        acTemperature = 0
+        startTime = 0
+        energyConsumption = 0
+        return startTime, acTemperature, energyConsumption
 
+
+m = Model()
+startTime, acTemperature, energyConsumption = m.roomTemperatureSetting(30, 26)
+print("start at", startTime, "hours earlier with ac temperature set to", acTemperature, "degree, cost", energyConsumption, "watt")
